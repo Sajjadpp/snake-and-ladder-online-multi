710368c949be5c1c260db2438d3d045e9783d377
Sajjadpp <sajjadmuhammed227@gmail.com>
2025-11-09 22:25:39 +0530

    changed the logo of the base page

--- client/src/assets/GameLogo/GameLogo.jsx
+++ client/src/assets/GameLogo/GameLogo.jsx
@@ -5,24 +5,10 @@ import { ANIMATION_VARIANTS } from '../../utils/constants';
 const GameLogo = () => {
   return (
     <motion.div
-      className="mb-8"
+      className="mb-0"
       variants={ANIMATION_VARIANTS.item}
     >
-      <div className="w-24 h-24 bg-white/15 backdrop-blur-sm rounded-2xl flex items-center justify-center shadow-lg">
-        <motion.div
-          className="text-4xl"
-          animate={{ 
-            rotate: [0, 5, -5, 0]
-          }}
-          transition={{ 
-            duration: 4,
-            repeat: Infinity,
-            ease: "easeInOut"
-          }}
-        >
-          🐍
-        </motion.div>
-      </div>
+    <img src="/snake&ladder.png" height={200} width={200} alt="" />
     </motion.div>
   );
 };
